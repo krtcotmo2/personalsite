cb6fbe318e6865a9c105f99e3c3b36285512b9d2
krtcotmo2 <krtcotmo2@gmail.com>
2019-05-12 17:03:32 -0400

    build files

--- src/Pages/Representation/Representation.js
+++ src/Pages/Representation/Representation.js
@@ -8,23 +8,36 @@ export default function Representation() {
       <h4>Representation</h4>
       <Carousel options={{ fullWidth: true, indicators: true }} className="center col s7">
         <div className="">
-          <h2>First Panel</h2>
+          <h2>Inventory Management</h2>
           <p>This is your first panel</p>
+          <a href="https://count-stockula.herokuapp.com/Scan">Inventory Management</a>
         </div>
         <div className="">
-          <h2>Second Panel</h2>
+          <h2>Car Maintenace Application</h2>
           <p>This is your second panel</p>
+          <a href="https://autoproject2.herokuapp.com/">Car Maintenace Application</a>
         </div>
         <div className="">
-          <h2>Third Panel</h2>
+          <h2>Rect Library with Favorites</h2>
           <p>This is your second panel</p>
+          <a href="https://react-myreading-library.herokuapp.com/">Rect Library with Favorites</a>
         </div>
         <div className="">
-          <h2>Fourth Panel</h2>
+          <h2>NFL Draft Board</h2>
           <p>This is your second panel</p>
-        </div>      
+          <a href="https://nfldraft-krc.herokuapp.com/">NFL Draft Board</a>
+        </div>  
+        <div className="">
+          <h2>Connectwise Commission Report</h2>
+          <p>This is your second panel</p>
+          <a href="https://nfldraft-krc.herokuapp.com/">Connectwise Commission Report</a>
+        </div>  
+        <div className="">
+          <h2>Connectwise Capacity Charts</h2>
+          <p>This is your second panel</p>
+          <a href="https://nfldraft-krc.herokuapp.com/">Connectwise Capacity Charts</a>
+        </div>    
       </Carousel>
-
       <img className="mernQuad" alt="MERN Stack" src="./images/mernQuads.png" />
       <div className="tagLine">React</div>
     </div>
