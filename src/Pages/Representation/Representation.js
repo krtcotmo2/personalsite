9580892889bae87968ea520304c4993c0f64ec08
krtcotmo2 <krtcotmo2@gmail.com>
2019-05-12 17:27:16 -0400

    updated links to window out added material icons in main css

--- src/Pages/Representation/Representation.js
+++ src/Pages/Representation/Representation.js
@@ -10,32 +10,32 @@ export default function Representation() {
         <div className="">
           <h2>Inventory Management</h2>
           <p>This is your first panel</p>
-          <a href="https://count-stockula.herokuapp.com/Scan">Inventory Management</a>
+          <a target='_blank' href="https://count-stockula.herokuapp.com/Scan">Inventory Management</a>
         </div>
         <div className="">
           <h2>Car Maintenace Application</h2>
           <p>This is your second panel</p>
-          <a href="https://autoproject2.herokuapp.com/">Car Maintenace Application</a>
+          <a target='_blank' href="https://autoproject2.herokuapp.com/">Car Maintenace Application</a>
         </div>
         <div className="">
           <h2>Rect Library with Favorites</h2>
           <p>This is your second panel</p>
-          <a href="https://react-myreading-library.herokuapp.com/">Rect Library with Favorites</a>
+          <a target='_blank' href="https://react-myreading-library.herokuapp.com/">Rect Library with Favorites</a>
         </div>
         <div className="">
           <h2>NFL Draft Board</h2>
           <p>This is your second panel</p>
-          <a href="https://nfldraft-krc.herokuapp.com/">NFL Draft Board</a>
+          <a target='_blank' href="https://nfldraft-krc.herokuapp.com/">NFL Draft Board</a>
         </div>  
         <div className="">
           <h2>Connectwise Commission Report</h2>
           <p>This is your second panel</p>
-          <a href="https://nfldraft-krc.herokuapp.com/">Connectwise Commission Report</a>
+          <a target='_blank' href="https://nfldraft-krc.herokuapp.com/">Connectwise Commission Report</a>
         </div>  
         <div className="">
           <h2>Connectwise Capacity Charts</h2>
           <p>This is your second panel</p>
-          <a href="https://nfldraft-krc.herokuapp.com/">Connectwise Capacity Charts</a>
+          <a target='_blank' href="https://nfldraft-krc.herokuapp.com/">Connectwise Capacity Charts</a>
         </div>    
       </Carousel>
       <img className="mernQuad" alt="MERN Stack" src="./images/mernQuads.png" />
